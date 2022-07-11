30beb443a4bfcd810cadec7fec3179ddfd7cb4c4
Laura Fialkovska <laura.fialkovska@gmail.com>
2022-07-11 18:16:03 +0300

    Homework 2022.07.07

--- src/homework_20220707/hw2/Action.java
+++ src/homework_20220707/hw2/Action.java
@@ -5,53 +5,54 @@ import java.util.Random;
 public class Action {
     Random rand = new Random();
 	
-	// Knight attack power(1-8)
+	// Knight attack power (1-8)
     public int knightPow() {
         int attRatio = rand.nextInt(8);
         return 1 + attRatio;
     }
 	
-    // Barbarian attack power(1-6)
+    // Barbarian attack power (1-6)
     public int barbarPow() {
         int attRatio = rand.nextInt(5);
         return 2 + attRatio;
     }
 	
-    // Wizard attack power(3-4)
+    // Wizard attack power (3-4)
     public int wizardPow() {
-        return 3;
+        int attRatio = rand.nextInt(2);
+        return 3 + attRatio;
     }
 	
-    // Knight blocks PC attack(4-7)
+    // Knight blocks PC attack (4-7)
     public int knightBlock() {
         int attRatio = rand.nextInt(4);
         return 4 + attRatio;
     }
 	
-    // Barbarian blocks PC attack(3-5)
+    // Barbarian blocks PC attack (3-5)
     public int barbarBlock() {
         int attRatio = rand.nextInt(3);
         return 3 + attRatio;
     }
 	
-    // Wizard blocks PC attack(1-2)
+    // Wizard blocks PC attack (1-2)
     public int wizardBlock() {
         int attRatio = rand.nextInt(2);
         return 1 + attRatio;
     }
 	
-    // Knight regenerates health points(1)
+    // Knight regenerates health points (1)
     public int knightReg() {
         return 1;
     }
 	
-    // Barbarian regenerates health points(2-3)
+    // Barbarian regenerates health points (2-3)
     public int barbarReg() {
-        int attRatio = rand.nextInt(1);
+        int attRatio = rand.nextInt(2);
         return 2 + attRatio;
     }
 	
-    // Wizard regenerates health points(3-5)
+    // Wizard regenerates health points (3-5)
     public int wizardReg() {
         int attRatio = rand.nextInt(3);
         return 3 + attRatio;
--- src/homework_20220707/hw2/Main.java
+++ src/homework_20220707/hw2/Main.java
@@ -1,89 +1,114 @@
 package homework_20220707.hw2;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Prompter prompter = new Prompter();
         Random rand = new Random();
-        Scanner scan = new Scanner(System.in);
 
 		// Speletaja dati
         int userHp = 14;
+        int attackCounter = 0;
+        int bonusAmount = 0;
+        boolean payBonus = false;
+
 		// PC dati
         int pcHp = 14;
         int pcAtt = 2;
-        int pcReg = 2;
+        // int pcReg = 2;
 
         String warrior = prompter.askForWarrior();
         System.out.println("\n" + ".:: Let the battle begins ::.");
 
         while (pcHp > 0) {
             String action = prompter.askForAction();
-            Action userAttRand = new Action();
+            Action battle = new Action();
 			
             int userAtt = 0;
             int userBlock = 0;
             int userReg = 0;
+
 			// PC attack ratio (2-6)
             int pcAttRand = rand.nextInt(5);
-			
+            int pcAttRatio = pcAtt + pcAttRand;
+
+            // Store max PC attack ratio as a bonus for user bravery
+            if (pcAttRatio > bonusAmount) {
+                bonusAmount = pcAttRatio;
+            }
+
 			// Players updated stats
             switch (warrior) {
                 case "k":
-                    userAtt = userAttRand.knightPow();
-                    userBlock = userAttRand.knightBlock();
-                    userReg = userAttRand.knightReg();
+                    userAtt = battle.knightPow();
+                    userBlock = battle.knightBlock();
+                    userReg = battle.knightReg();
                     break;
                 case "b":
-                    userAtt = userAttRand.barbarPow();
-                    userBlock = userAttRand.barbarBlock();
-                    userReg = userAttRand.barbarReg();
+                    userAtt = battle.barbarPow();
+                    userBlock = battle.barbarBlock();
+                    userReg = battle.barbarReg();
                     break;
                 case "w":
-                    userAtt = userAttRand.wizardPow();
-                    userBlock = userAttRand.barbarBlock();
-                    userReg = userAttRand.wizardReg();
+                    userAtt = battle.wizardPow();
+                    userBlock = battle.barbarBlock();
+                    userReg = battle.wizardReg();
                     break;
             }
 			
             switch (action) {
 				// Attack description
                 case "a":
+                    attackCounter += 1;
+
                     pcHp = pcHp - userAtt;
-                    System.out.println("\n" + "You hit " + userAtt + " points" );
-                    System.out.println("PC left " + pcHp + " health points");
-                    
-					userHp = userHp - (pcAtt + pcAttRand);
-                    System.out.println("\n" + "PC hit " + (pcAtt + pcAttRand) + " points" );
-                    System.out.println("User left " + userHp + " health points");
+                    System.out.println("\n" + "You hit " + userAtt + " points." );
+                    System.out.println("PC left " + pcHp + " health points.");
+
+                    userHp = userHp - pcAttRatio;
+                    System.out.println("\n" + "PC hit " + pcAttRatio + " points." );
+                    System.out.println("User left " + userHp + " health points.");
+
                     break;
 				// Block description
                 case "b":
+                    attackCounter = 0;
                     userHp = userHp + userReg;
-					
-					if ((pcAtt + pcAttRand) > userBlock) {
-						userHp = userHp - ((pcAtt + pcAttRand) - userBlock);
-						System.out.println("\n" + "You used block, but PC was stronger and dameged your HP by " +
-                                ((pcAtt + pcAttRand) - userBlock));
-						System.out.println("User left " + userHp + " health points");
-					} else {
-						System.out.println("\n" + "You block PC's attack and regenerated " + userReg + " HP.");
-						System.out.println("User left " + userHp + " health points");
-					}
-					
-                    System.out.println("\n" + "User left " + userHp + " health points");
+
+                    if (pcAttRatio > userBlock) {
+                        userHp = userHp - (pcAttRatio - userBlock);
+                        System.out.println("\n" + "You used block, but PC was stronger and damaged your HP by " +
+                                (pcAttRatio - userBlock) + ".");
+                        System.out.println("User left " + userHp + " health points.");
+                    } else {
+                        System.out.println("\n" + "You block PC's attack and regenerated " + userReg + " HP.");
+                        System.out.println("User left " + userHp + " health points.");
+                    }
+
                     break;
             }
-			
-			if (pcHp <= 0) {
-				System.out.println("\n" + "You won!");
-				break;
-			} else if (userHp <= 0) {
-				System.out.println("\n" + "You lost!");
-				break;
-			}
+
+            if (pcHp <= 0) {
+                System.out.println("\n" + "You won!");
+                break;
+            }
+
+            if (attackCounter >= 3) {
+                payBonus = true;
+            }
+
+            // If the user is close to death and has once shown courage
+			if (userHp <= 0 && payBonus) {
+                userHp += bonusAmount;
+
+				System.out.println("\n" + "You almost lost!");
+                System.out.println("But thanks to your courage you get a second life!");
+                System.out.println("User left " + userHp + " health points.");
+			} else if (userHp <= 0 && !payBonus) {
+                System.out.println("\n" + "You lost!");
+                break;
+            }
         }
     }
 }
