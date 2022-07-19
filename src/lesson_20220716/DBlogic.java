abeae1d96a1915de97ab2e6f1ead143f9a43ef62
Laura Fialkovska <laura.fialkovska@gmail.com>
2022-07-19 18:20:22 +0300

    Lesson 2022.07.16

---
+++ src/lesson_20220716/DBlogic.java
@@ -0,0 +1,68 @@
+package lesson_20220716;
+
+import javax.xml.namespace.QName;
+import java.sql.*;
+
+public class DBlogic {
+    private String DB = "jdbc:mysql://localhost:3306/07_crud";
+    private String USER = "root";
+    private String PASSWORD = "mySQLR00tpsw";
+
+    public void insert() {
+        try {
+            Connection cnctn = DriverManager.getConnection(DB, USER, PASSWORD);
+            Statement stmt = cnctn.createStatement(); // SQL to DB
+
+            String sqlInsert = "INSERT INTO users (email, name) VALUES ('lf@gmail.com', 'Laura')";
+            stmt.executeUpdate(sqlInsert);
+            System.out.println("Insert: done!");
+        } catch (SQLException excptn) {
+            excptn.printStackTrace();
+        }
+    }
+
+    public void update() {
+        try {
+            Connection cnctn = DriverManager.getConnection(DB, USER, PASSWORD);
+            Statement stmt = cnctn.createStatement();
+
+            String sqlUpdate = "UPDATE users SET email = 'newlf@gmail.com' WHERE id = 2 LIMIT 1";
+            stmt.executeUpdate(sqlUpdate);
+            System.out.println("Update: done!");
+        } catch (SQLException excptn) {
+            excptn.printStackTrace();
+        }
+    }
+
+    public void delete() {
+        try {
+            Connection cnctn = DriverManager.getConnection(DB, USER, PASSWORD);
+            Statement stmt = cnctn.createStatement();
+
+            String sqlDelete = "DELETE FROM users WHERE id = 1 LIMIT 1";
+            stmt.executeUpdate(sqlDelete);
+            System.out.println("Delete: done!");
+        } catch (SQLException excptn) {
+            excptn.printStackTrace();
+        }
+    }
+
+    public void select() {
+        try {
+            Connection cnctn = DriverManager.getConnection(DB, USER, PASSWORD);
+            Statement stmt = cnctn.createStatement();
+            ResultSet resultSet = stmt.executeQuery("SELECT id, email, name FROM users");
+
+            // next(): kamēr tabulā ir ieraksti, izpildam kodu..
+            while (resultSet.next()) {
+                System.out.println("id: " + resultSet.getInt("id") +
+                        "; e-mail: " + resultSet.getString("email") +
+                        "; name: " + resultSet.getString("name"));
+            }
+
+            System.out.println("\n" + "Select: done!");
+        } catch (SQLException excptn) {
+            excptn.printStackTrace();
+        }
+    }
+}
---
+++ src/lesson_20220716/Main.java
@@ -0,0 +1,12 @@
+package lesson_20220716;
+
+public class Main {
+    public static void main(String[] args) {
+        DBlogic db = new DBlogic();
+
+//        db.insert();
+//        db.delete();
+//        db.update();
+        db.select();
+    }
+}
