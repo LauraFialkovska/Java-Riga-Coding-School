47d438798c5d30dc45b05aeb6ee10f920d19abe6
Laura Fialkovska <laura.fialkovska@gmail.com>
2022-08-16 21:35:00 +0300

    Lesson 2022.08.16

---
+++ src/lesson_20220816/Hashing.java
@@ -0,0 +1,40 @@
+package lesson_20220816;
+
+public class Hashing {
+    public static void main(String[] args) {
+
+        String password = "madars";
+        System.out.println("Password: " + password);
+
+        String hashedPassword = hash(password);
+        System.out.println("hashedPassword: " + hashedPassword);
+
+        String unhashedPassword = unhash(hashedPassword);
+        System.out.println("unhashedPassword: " + unhashedPassword);
+    }
+
+    static String hash(String password) {
+        // ASCII characters
+        char[] characters = password.toCharArray();
+        String hashedPassword = "";
+
+        for (char c : characters) {
+            c += 1;
+            hashedPassword += c;
+        }
+
+        return hashedPassword;
+    }
+
+    static String unhash(String password) {
+        char[] characters = password.toCharArray();
+        String unhashedPassword = "";
+
+        for (char c : characters) {
+            c -= 1;
+            unhashedPassword += c;
+        }
+
+        return unhashedPassword;
+    }
+}
